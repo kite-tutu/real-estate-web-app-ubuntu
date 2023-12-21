19ce190990b3f52c9c3dd9ca6e3f8a7d1bffb697
Okiemute Agbatutu <kite_tutu@yahoo.com>
2023-12-21 22:05:58 +0100

    Added Not Found 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import PropertiesPage from './pages/PropertiesPage';
+import NotFound from './components/NotFound';
 
 const App = ()=> {
   return (
@@ -16,6 +18,7 @@ const App = ()=> {
             <Route path='/' element={<HomePage/>}/>
       
             <Route path='/properties' element={<PropertiesPage/>}/>
+            <Route path='*' element={<NotFound/>}/>
           </Routes>
           <ToastContainer/>
         </main>
